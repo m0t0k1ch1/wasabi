5b9b16fd96713a184198bc7899a391843202a392
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-11-24 21:56:34 +0900

    update response text

--- action_show.go
+++ action_show.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/m0t0k1ch1/potto"
@@ -19,7 +20,7 @@ func Show(pctx potto.Ctx, args potto.ActionArgs) (*potto.Response, error) {
 	case len(members) == 0:
 		text = "no member"
 	default:
-		text = strings.Join(members, ", ")
+		text = fmt.Sprintf("targets: %s", strings.Join(members, ", "))
 	}
 
 	return response(text)
